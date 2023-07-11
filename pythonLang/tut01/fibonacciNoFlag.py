02e26010bb924e11051ec06875957925e0949bc3
cmadmin <mirkomega10@gmail.com>
2023-07-11 18:39:29 +0200

    aggiornato fibonacci

--- pythonLang/tut01/fibonacciNoFlag.py
+++ pythonLang/tut01/fibonacciNoFlag.py
@@ -4,7 +4,7 @@ import time
 # code
 def fibonacci_of(n):
     if n <= 0:
-        print("\n<<Please enter a positive integer.>>")
+        print("\n<<Please enter a [positive integer].>>")
     n1, n2 = 0, 1
     tic = time.perf_counter()
     for i in range(n + 1):
@@ -15,22 +15,28 @@ def fibonacci_of(n):
         n1 = n2
         n2 = fibnum
     toc = time.perf_counter()
-    print(f"\nLast number = {fibnum}") # print only last number to make calculations faster
-    print(f"\n<<Code executed in {toc - tic:0.5f} seconds>>")
+    time = tic - toc
+    print(f"\n<<Last number>> = {fibnum}") # print only last number to make calculations faster
+    print(f"\n<<Code executed in {time:0.5f} seconds>>")
 
-def request(repeat_flag):
-    if repeat_flag == 0: # the first time you won't be asked
-        accept = str(input("\n<<Do you want to execute the program? y/n -> "))
-    if accept == "y" or repeat_flag == 0:
+def print_():
+     try:
         n = int(input("\n<<How many times do you want to calculate the sequence?>>: "))
         fibonacci_of(n - 2) # -2 why 0 & 1 are excluded
         repeat_flag = 1 # checks if you have already executed the code
-        request(repeat_flag)
+        request()
+     except:
+        print("\n<<Please insert an [integer]>>")
+        print_() # print_ function created to work around the problem of requesting execution
+
+def request():
+    accept = str(input("\n<<Do you want to execute the program? y/n -> "))
+    if accept == "y":
+        print_()
     elif accept == "n":
-        print("\n<<The program won't be executed again.>>")
+        print("\n<<The program won't be executed.>>")
     else:
         print("\n<<Please define one of the two possible statements.>>")
-        request(repeat_flag)
+        request()
 
-repeat_flag = 0
-request(repeat_flag)
+request()

31d828f6010ca1c1754072f635029b052bd7c730
MMeg4 <mirkomega10@gmail.com>
2023-07-12 11:33:56 +0200

    completato fibonacci, rimossa seconda versione

--- pythonLang/tut01/fibonacci.py
+++ pythonLang/tut01/fibonacci.py
@@ -11,22 +11,30 @@ def fibonacci_of(n):
        #if i == 0: # excluded to print only the final Fibonacci num
             #print(0)
         fibnum = n1 + n2
-        #print(fibnum) #
+        #print(fibnum) # excluded to print only the final Fibonacci num
         n1 = n2
         n2 = fibnum
     toc = time.perf_counter()
     print(f"\nLast number = {fibnum}") # print only last number to make calculations faster
     print(f"\n<<Code executed in {toc - tic:0.5f} seconds>>")
 
-def request(repeat_flag):
-    if repeat_flag == 0: # the first time you won't be asked
-        accept = str(input("\n<<Do you want to execute the program? y/n -> "))
-    if accept == "y" or repeat_flag == 0:
+def print_():
+    try:
         n = int(input("\n<<How many times do you want to calculate the sequence?>>: "))
         fibonacci_of(n - 2) # -2 why 0 & 1 are excluded
         repeat_flag = 1 # checks if you have already executed the code
         request(repeat_flag)
-    elif accept == "n":
+    except:
+        print("\n<<Please insert an [integer]>>")
+        print_() # print_ function created to work around the problem of requesting execution
+
+def request(repeat_flag):
+    accept = ""
+    if repeat_flag != 0: # the first time you won't be asked
+        accept = str(input("\n<<Do you want to execute the program again? y/n>> -> "))
+    if accept == "y" or accept == "Y" or repeat_flag == 0:
+        print_()
+    elif accept == "n" or accept == "N" :
         print("\n<<The program won't be executed again.>>")
     else:
         print("\n<<Please define one of the two possible statements.>>")
